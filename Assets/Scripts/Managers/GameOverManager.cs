f3e8ba9046df7ebaf2e6869bc79a58ffc0e20420
山谷 優介 <yusuke.yamatani@000338-M2.local>
2015-03-20 00:07:58 +0900

    Initial commit

---
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class GameOverManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+	public float restartDelay = 5f;
+	public AudioSource BackgroundMugic ;
+
+
+    Animator anim;
+	float restartTimer;
+	
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+//		BackgroundMugic = GetComponent <AudioSource> ();
+    }
+
+
+    void Update()
+    {
+        if (playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("GameOver");
+
+			BackgroundMugic.Stop();
+
+			restartTimer += Time.deltaTime;
+
+			if(restartTimer >= restartDelay)
+			{
+				Application.LoadLevel(Application.loadedLevel );
+			}
+        }
+    }
+}
